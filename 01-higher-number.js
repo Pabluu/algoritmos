42425927910931a6e3ca03fbdedb6fd568351a37
Pablo Henrique SC <pabloph0077@gmail.com>
2023-08-28 14:46:57 -0300

    feat: problem resolution

--- 01-higher-number.js
+++ 01-higher-number.js
@@ -5,6 +5,16 @@ Exemplo: se a = 2 e b = 7, o algoritmo devolve 7.
 RESTRIÇÕES:
   * Não pode usar if's, estruturas de comparação, operador ternário, bibliotecas tipo Math, Colections ou semelhantes.
 */
+
+// Função que retorna o maior numero.
+function higherNumber(a, b) {
+  soma = a + b
+  sub = abs(a - b)
+  result = (soma + sub) / 2
+
+  return result
+}
+
 // Função que retorna o modulo de um numero
 function abs(a) {
   if (a < 0) {
@@ -13,3 +23,6 @@ function abs(a) {
 
   return a
 }
+
+higherNumber(2, 7) // 7
+higherNumber(3, 1) // 3
