cc79751f841546b14167248eb4ababa2d3761d05
Pablo Henrique SC <pabloph0077@gmail.com>
2023-08-28 14:45:09 -0300

    doc: problem description

---
+++ 01-higher-number.js
@@ -0,0 +1,7 @@
+/*
+Desenvolver um algoritmo que receba 2 variáveis a e b do tipo inteiro e devolve o maior deles.
+Exemplo: se a = 2 e b = 7, o algoritmo devolve 7.
+
+RESTRIÇÕES:
+  * Não pode usar if's, estruturas de comparação, operador ternário, bibliotecas tipo Math, Colections ou semelhantes.
+*/
